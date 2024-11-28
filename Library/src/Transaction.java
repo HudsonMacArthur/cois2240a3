f270dd87c23989a37aa7cfe1a9bf6c7f58f0583b
HudsonMacArthur <hudsonmacarthur@trentu.ca>
2024-11-27 21:16:34 -0500

    added Transaction.saveTransaction() to write to file

--- Library/src/Transaction.java
+++ Library/src/Transaction.java
@@ -1,5 +1,7 @@
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Transaction {
 	
@@ -23,6 +25,10 @@ public class Transaction {
             member.borrowBook(book); 
             String transactionDetails = getCurrentDateTime() + " - Borrowing: " + member.getName() + " borrowed " + book.getTitle();
             System.out.println(transactionDetails);
+            
+            // save this transaction to file
+            saveTransaction(transactionDetails);
+            
             return true;
         } else {
             System.out.println("The book is not available.");
@@ -37,6 +43,10 @@ public class Transaction {
             book.returnBook();
             String transactionDetails = getCurrentDateTime() + " - Returning: " + member.getName() + " returned " + book.getTitle();
             System.out.println(transactionDetails);
+            
+            // save this transaction to file
+            saveTransaction(transactionDetails);
+            
         } else {
             System.out.println("This book was not borrowed by the member.");
         }
@@ -47,4 +57,21 @@ public class Transaction {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         return sdf.format(new Date());
     }
+	
+	// Save a transaction to file
+	private void saveTransaction(String transactionDetails)
+	{
+		try
+		{
+			FileWriter writer = new FileWriter("transactions.txt", true);
+			
+			// write to transactions.txt
+			writer.write(transactionDetails + "\n");
+			writer.close();
+		}
+		catch (IOException e)
+		{
+			System.out.println(e.getMessage());
+		}
+	}
 }
